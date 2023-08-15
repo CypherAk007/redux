e97dcc8bfb058877107b140c6ec540312a45e7df
CypherAk007 <ktmabhi007@gmail.com>
2023-08-15 16:26:10 +0530

    redux basics

---
+++ redux-demo.js
@@ -0,0 +1,32 @@
+const redux = require('redux')
+
+// 2-> create reducer fnc
+const counterReducer = (state = {counter:0},action)=>{
+  if (action.type==='increment'){
+    return {
+      counter:state.counter+1
+    }
+  }
+  if (action.type==='decrement'){
+    return{
+      counter:state.counter-1
+    }
+  }
+}
+
+//1-> create store
+const store = redux.createStore(counterReducer)
+
+// 3-> create subscription
+
+const counterSubscribe = ()=>{
+  const latestState = store.getState()
+  console.log(latestState);
+}
+
+// 4-> redux on subscriber
+store.subscribe(counterSubscribe)
+
+store.dispatch({type:'decrement'})
+store.dispatch({type:'increment'})
+
